8ae55e1237cb8072616044622e1cc547a93700c1
Missouriiii <112442506+Missouriiii@users.noreply.github.com>
2022-09-04 15:36:07 -0400

    Update Main.php

--- src/Laith98Dev/FFA/Main.php
+++ src/Laith98Dev/FFA/Main.php
@@ -672,17 +672,3 @@ class Main extends PluginBase implements Listener
 		});
 	}
 }
-Footer
-©2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
